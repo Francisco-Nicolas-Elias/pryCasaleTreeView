c25fe7fd4c902c4c93b3a0cdfd86c4aa6f148106
Francisco Elias <franeliasbv@gmail.com>
2023-09-05 14:38:07 -0300

    Agregar archivos de proyecto.

---
+++ pryCasaleTreeView/main.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace pryCasaleTreeView
+{
+    partial class main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.splitExplorador = new System.Windows.Forms.SplitContainer();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tipo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.detalle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.splitExplorador)).BeginInit();
+            this.splitExplorador.Panel1.SuspendLayout();
+            this.splitExplorador.Panel2.SuspendLayout();
+            this.splitExplorador.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitExplorador
+            // 
+            this.splitExplorador.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitExplorador.Location = new System.Drawing.Point(0, 0);
+            this.splitExplorador.Name = "splitExplorador";
+            // 
+            // splitExplorador.Panel1
+            // 
+            this.splitExplorador.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitExplorador.Panel2
+            // 
+            this.splitExplorador.Panel2.Controls.Add(this.listView1);
+            this.splitExplorador.Size = new System.Drawing.Size(574, 450);
+            this.splitExplorador.SplitterDistance = 191;
+            this.splitExplorador.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "file.png");
+            this.imageList1.Images.SetKeyName(1, "folder.jpg");
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(191, 450);
+            this.treeView1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nombre,
+            this.tipo,
+            this.detalle});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(379, 450);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // nombre
+            // 
+            this.nombre.Text = "nombre";
+            // 
+            // tipo
+            // 
+            this.tipo.Text = "tipo";
+            // 
+            // detalle
+            // 
+            this.detalle.Text = "detalle";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 450);
+            this.Controls.Add(this.splitExplorador);
+            this.Name = "main";
+            this.Text = "Uso de controles - TreeView";
+            this.splitExplorador.Panel1.ResumeLayout(false);
+            this.splitExplorador.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitExplorador)).EndInit();
+            this.splitExplorador.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitExplorador;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader nombre;
+        private System.Windows.Forms.ColumnHeader tipo;
+        private System.Windows.Forms.ColumnHeader detalle;
+    }
+}
+
---
+++ pryCasaleTreeView/main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryCasaleTreeView
+{
+    public partial class main : Form
+    {
+        public main() // constructor
+        {
+            InitializeComponent();
+            LlenarTreeView();
+        }
+
+        private void LlenarTreeView()
+        {
+            TreeNode nodoMadre;
+
+            DirectoryInfo info = new DirectoryInfo(@"E:/Documentos");
+            if (info.Exists == true) //POR DEFECTO el IF pregunta true
+            {
+                nodoMadre = new TreeNode(info.Name);
+                nodoMadre.Tag = info;
+                ObtenerCarpetas(info.GetDirectories(), nodoMadre);
+                treeView1.Nodes.Add(nodoMadre);
+            }
+        }
+
+        //desde info.GetDirectories() nos da todos los nombrs
+        //de carpetas
+        private void ObtenerCarpetas(DirectoryInfo[] subDirs,
+    TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "folder";
+                
+                //recursiva - se llama a si mismo para
+                //buscar màs carpetas
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    ObtenerCarpetas(subSubDirs, aNode);
+                }
+
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+        }
+    }
+    //https://learn.microsoft.com/es-es/dotnet/desktop/winforms/controls/creating-an-explorer-style-interface-with-the-listview-and-treeview?view=netframeworkdesktop-4.8
+}
+
